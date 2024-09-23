5b4fff4ca8c0a1eda6c98d6ad86d9139430d7319
juliareinke <juliareinke29@icloud.com>
2024-09-22 21:01:54 -0300

    fix react router

--- src/Main.jsx
+++ src/Main.jsx
@@ -1,25 +1,16 @@
 import ReactDOM from "react-dom/client";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 
 import Home from "./paginas/home/Home";
 import NotFound from "./paginas/not-found/NotFound";
 import Detalhes from "./paginas/detalhes/Detalhes";
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Home />,
-  },
-  {
-    path: "/detalhes/:id",
-    element: <Detalhes />,
-  },
-  {
-    path: "*",
-    element: <NotFound />,
-  },
-]);
-
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <RouterProvider router={router} />
+  <HashRouter>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/detalhes/:id" element={<Detalhes />} />
+      <Route path="*" element={<NotFound />} />
+    </Routes>
+  </HashRouter>
 );
--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/at_fund_react/"
+  base: '/at_fund_react/',
 })
